ff5972318943b3ddf09a0434165ab7204ec78508
ChristianVisintin <christian.visintin1997@gmail.com>
2020-06-13 16:59:58 +0200

    File utils write_lines()

--- src/utils/file.rs
+++ src/utils/file.rs
@@ -24,7 +24,8 @@
 */
 
 use std::fs::File;
-use std::io::{self, BufRead};
+use std::fs::OpenOptions;
+use std::io::{self, BufRead, Write};
 use std::path::Path;
 
 /// ### read_lines
@@ -40,6 +41,21 @@ pub fn read_lines<P>(filename: P) -> io::Result<Vec<String>> where P: AsRef<Path
         }
     }
     Ok(lines)
+}
+
+/// ### write_lines
+/// 
+/// Write lines to file
+pub fn write_lines<P>(filename: P, lines: Vec<String>) -> io::Result<()> where P: AsRef<Path> {
+    match OpenOptions::new().create(true).write(true).append(false).truncate(true).open(filename) {
+        Ok(mut f) => {
+            for line in lines.iter() {
+                writeln!(f, "{}", line)?;
+            }
+            Ok(())
+        },
+        Err(err) => Err(err)
+    }
 }
 
 #[cfg(test)]
@@ -62,6 +78,24 @@ mod tests {
     #[test]
     fn test_utils_file_read_lines_no_file() {
         assert!(read_lines(Path::new("/sample.file123123.txt")).is_err());
+    }
+
+    #[test]
+    fn test_utils_file_write_lines() {
+        let in_lines: Vec<String> = vec![String::from("row 1"), String::from("row 2"), String::from("row 3")];
+        let tmpfile: tempfile::NamedTempFile = tempfile::NamedTempFile::new().unwrap();
+        assert!(write_lines(tmpfile.path(), in_lines.clone()).is_ok());
+        //Verify rows
+        let res: io::Result<Vec<String>> = read_lines(tmpfile.path());
+        assert!(res.is_ok());
+        let out_lines: Vec<String> = res.unwrap();
+        assert_eq!(in_lines, out_lines);
+    }
+
+    #[test]
+    fn test_utils_file_write_lines_error() {
+        let in_lines: Vec<String> = vec![String::from("row 1"), String::from("row 2"), String::from("row 3")];
+        assert!(write_lines(Path::new("/sample.file1231234.txt"), in_lines).is_err());
     }
 
     /// ### write_sample_file
