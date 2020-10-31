1fb0f85600f276e5e4a7738dea38c1a30c6b8d2e
ChristianVisintin <christian.visintin1997@gmail.com>
2020-10-31 10:15:24 +0100

    Fixed directive to ignore macos test for tarpaulin

--- src/shell/unixsignal.rs
+++ src/shell/unixsignal.rs
@@ -110,7 +110,7 @@ impl UnixSignal {
     }
 
     #[cfg(target_os = "macos")]
-    #[cfg_attr(tarpaulin, skip)]
+    #[cfg(not(tarpaulin_include))]
     /// ### to_nix_signal
     /// 
     /// Converts a UnixSignal to a nix::signal
